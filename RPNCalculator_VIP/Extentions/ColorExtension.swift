cb790144bab32c03918a31ec1226163e2367827b
Abdulvoxid <abdulvohidmahmudov55@gmail.com>
2025-03-24 20:44:02 +0500

    updated

---
+++ RPNCalculator_VIP/Extentions/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  RPNCalculator_VIP
+//
+//  Created by Abdulvoxid on 24/03/25.
+//
+
+import Foundation
--- RPNCalculator_VIP/Services/HistoryService.swift
+++ RPNCalculator_VIP/Services/HistoryService.swift
@@ -11,6 +11,7 @@ import CoreData
 protocol HistoryServiceProtocol {
     func saveHistory(result: String, expression: String)
     func fetchHistory() -> [(String, String)]
+    //func deleteItem(indexPath: IndexPath)
 }
 
 final class HistoryService: HistoryServiceProtocol {
@@ -45,6 +46,20 @@ final class HistoryService: HistoryServiceProtocol {
         }
     }
        
+    
+//    func deleteItem(savedData: [(String,String)],indexPath: IndexPath) {
+//        let objectToDelete = savedData[indexPath.row]
+//        
+//        context.delete(objectToDelete) // Delete from Core Data
+//        savedData.remove(at: indexPath.row) // Remove from array
+//        
+//        do {
+//            try context.save() // Save changes
+//            tableView.deleteRows(at: [indexPath], with: .fade) // Remove row with animation
+//        } catch {
+//            print("Failed to delete: \(error)")
+//        }
+//    }
     
     
 }
